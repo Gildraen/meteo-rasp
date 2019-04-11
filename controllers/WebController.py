a2659e47fea9bbbf213ed6880f505c2f21124466
Gildraen <gaetan.coleno@free.fr>
2019-04-11 12:21:24 +0200

    affiche histo capteurs

--- controllers/WebController.py
+++ controllers/WebController.py
@@ -7,7 +7,7 @@ sys.path.append('/home/projects/meteo-rasp/entity')
 sys.path.append('/home/projects/meteo-rasp/repo')
 from caption_dao import CaptionDAO
 from collect_dao import CollectDAO
-
+from data import Data
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,9 +17,11 @@ def accueil():
 	data = {}
 
 	for capteur in capteurs:
-		collects = CollectDAO.getAll(macAddress=capteur.macAddress)
-		data[capteur] = collects
-
+		collectsHum = CollectDAO.getAll(macAddress=capteur.macAddress, DataId=Data.HUMIDITY)
+		collectsTemp = CollectDAO.getAll(macAddress=capteur.macAddress, DataId=Data.TEMPERATURE)
+		collectsBat = CollectDAO.getAll(macAddress=capteur.macAddress, DataId=Data.BATTERY)
+		print(len(collectsBat))
+		data[capteur] = dict({'temperature' : collectsTemp, 'humidity': collectsHum, 'battery':collectsBat})
 	return render_template('accueil.html', data=data)
 
 if __name__ == '__main__':
