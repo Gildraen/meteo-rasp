1f8a83682644ad076c92d456101396a771a244d7
Gildraen <gaetan.coleno@free.fr>
2019-04-11 10:16:36 +0200

    Webcontroller bug

--- controllers/WebController.py
+++ controllers/WebController.py
@@ -6,21 +6,21 @@ import sys
 sys.path.append('/home/projects/meteo-rasp/entity')
 sys.path.append('/home/projects/meteo-rasp/repo')
 from caption_dao import CaptionDAO
- 
+from collect_dao import CollectDAO
+
 app = Flask(__name__)
 
 @app.route('/')
 def accueil():
 
 	capteurs = CaptionDAO.getAll()
-    data = {}
-	
-	for capteur in capteurs
-		collects = CollectDAO.getAll(capteur.macAddress)
+	data = {}
+
+	for capteur in capteurs:
+		collects = CollectDAO.getAll(macAddress=capteur.macAddress)
 		data[capteur] = collects
 
-    return render_template('accueil.html', data=data)
-		
+	return render_template('accueil.html', data=data)
+
 if __name__ == '__main__':
-    app.run(debug=True)
-    print('tto')
+	app.run(debug=True)
