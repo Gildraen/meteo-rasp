c0f860feb2e39d0e19e333ece0329632681de5ce
Gildraen <gaetan.coleno@free.fr>
2019-04-11 15:30:29 +0200

    add name change

--- controllers/WebController.py
+++ controllers/WebController.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from repo.caption_dao import CaptionDAO
 from repo.collect_dao import CollectDAO
 from entity.data import Data
@@ -19,6 +19,20 @@ def accueil():
 		collectsBat = CollectDAO.getAll(macAddress=capteur.macAddress, dataId=Data.BATTERY)
 		data[capteur] = dict({'temperature' : collectsTemp, 'humidity': collectsHum, 'battery':collectsBat})
 	return render_template('accueil.html', data=data)
+
+@app.route('/name/<string:macAddress>', methods=['POST'])
+def changeName(macAddress):
+	capteur = CaptionDAO.getByMacAddress(macAddress)
+	print(request.form['name'])
+	capteur.name = request.form['name']
+	CaptionDAO.update(capteur)
+	return redirect("/")
+
+
+
+
+
+
 
 if __name__ == '__main__':
 	app.run(debug=False)
